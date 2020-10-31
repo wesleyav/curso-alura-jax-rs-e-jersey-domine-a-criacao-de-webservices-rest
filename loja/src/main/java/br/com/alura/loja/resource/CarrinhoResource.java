93141a4806b82770eca916c30b303a5208a24e99
Wesley <wesley.contato@hotmail.com>
2020-10-30 22:23:52 -0300

    Status code e a Interface Uniforme

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,10 +1,17 @@
 package br.com.alura.loja.resource;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
@@ -14,9 +21,19 @@ public class CarrinhoResource {
 
 	@Path("{id}")
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_XML)
 	public String busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
-		return carrinho.toJson();
+		return carrinho.toXML();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response adiciona(String conteudo) {
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		URI uri = URI.create("/carrinhos/" + carrinho.getId());
+		return Response.created(uri).build();
+
 	}
 }
--- loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,30 +1,35 @@
 package br.com.alura.loja;
 
-import java.net.URI;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sun.prism.impl.Disposer.Target;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import junit.framework.Assert;
 
 public class ClienteTest {
 
 	private HttpServer server;
+	private WebTarget target;
+	private Client client;
 
 	@Before
 	public void startaServidor() {
 		server = Servidor.inicializaServidor();
+		this.client = ClientBuilder.newClient();
+		this.target = client.target("http://localhost:8080");
 	}
 
 	@After
@@ -41,6 +46,22 @@ public class ClienteTest {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 
+	}
+	
+	public void testQueSuportaNovosCarrinhos() {
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314, "Microfone", 31, 1));
+		carrinho.setRua("Rua Vergeuri 3185");
+		carrinho.setCidade("São Paulo");
+		String xml = carrinho.toXML();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals(201, response.getStatus());
+		String location = response.getHeaderString("Location");
+		String conteudo = client.target(location).request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Microfone"));
+		
 	}
 
 }
