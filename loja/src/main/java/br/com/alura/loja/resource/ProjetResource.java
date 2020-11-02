90011bdf59b552ca64452dfbce41302f388751fb
Wesley <wesley.contato@hotmail.com>
2020-11-02 13:54:28 -0300

    Deletando projetos

--- loja/src/main/java/br/com/alura/loja/resource/ProjetResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/ProjetResource.java
@@ -1,9 +1,12 @@
 package br.com.alura.loja.resource;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import br.com.alura.loja.dao.ProjetoDAO;
 import br.com.alura.loja.modelo.Projeto;
@@ -17,4 +20,11 @@ public class ProjetResource {
 		Projeto projeto = new ProjetoDAO().busca(1l);
 		return projeto.toXML();
 	}
+	
+	@Path("{id}")
+    @DELETE
+    public Response removeProjeto(@PathParam("id") long id) {
+        new ProjetoDAO().remove(id);
+        return Response.ok().build();
+    }
 }
